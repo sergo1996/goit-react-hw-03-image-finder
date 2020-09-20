69ca5c073671f89dfa80706bb32dd0bfdf54dd54
sergo1996 <56196384+sergo1996@users.noreply.github.com>
2020-09-20 21:33:41 +0300

    Updates

---
+++ precache-manifest.7b77da88b2e14376b37671ae99787d77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7c5c9b3e3877c6d416abd3735621f7b",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "dd1f3e11aebc4c3fce5f",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.6f863349.chunk.css"
+  },
+  {
+    "revision": "9378aece207e21f7a572",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.5b259bab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.5b259bab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd1f3e11aebc4c3fce5f",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.0230b920.chunk.js"
+  },
+  {
+    "revision": "00ed64b4697a879a9e95",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.4e03020b.js"
+  }
+]);
